402f5171ecb21699027a3b411169600c973966f7
Kyle D <contact@KyleDiggs.com>
2020-04-14 16:13:23 -0700

    chat and floating bar setup

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,52 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Collapse from '../assets/collapsechat.png';
+import Close from '../assets/close.png';
+import DefProfilePhoto from '../assets/defaultprofile.png';
+import Send from '../assets/send.png';
 
 export default class Chat extends Component {
     render() {
         return (
             <div id="ChatWindow">
-        <div class="top">
-            <button class="collapse chatCollapse"><img src="./assets/collapsechat.png" /></button>
-            <button class="close chatDeactivate"><img src="./assets/close.png" /></button>
-        </div>
-        <div class="chat-messages">
-            <div class="chat-message to-me">
-                <img class="profile-photo" src="./assets/defaultprofile.png"/>
-                <p class="chat-content">This is a test message</p>
+                <div class="top">
+                    <button class="collapse chatCollapse"><img src={Collapse} /></button>
+                    <button class="close chatDeactivate"><img src={Close} /></button>
+                </div>
+                <div class="chat-messages">
+                    <div class="chat-message to-me">
+                        <img class="profile-photo" src={DefProfilePhoto}/>
+                        <p class="chat-content">This is a test message</p>
+                    </div>
+                    <div class="chat-message from-me">
+                        <img class="profile-photo" src={DefProfilePhoto}/>
+                        <p class="chat-content">This is a test message</p>
+                    </div>
+                    <div class="chat-message from-me">
+                        <img class="profile-photo" src={DefProfilePhoto}/>
+                        <p class="chat-content">This is a test message</p>
+                    </div>
+                    <div class="chat-message from-me">
+                        <img class="profile-photo" src={DefProfilePhoto}/>
+                        <p class="chat-content">This is a test message</p>
+                    </div>
+                    <div class="chat-message from-me">
+                        <img class="profile-photo" src={DefProfilePhoto}/>
+                        <p class="chat-content">This is a test message</p>
+                    </div>
+                    <div class="chat-message from-me">
+                        <img class="profile-photo" src={DefProfilePhoto}/>
+                        <p class="chat-content">This is a test message</p>
+                    </div>
+                    <div class="chat-message from-me">
+                        <img class="profile-photo" src={DefProfilePhoto}/>
+                        <p class="chat-content">This is a test message</p>
+                    </div>
+                    <div class="chat-message from-me">
+                        <img class="profile-photo" src={DefProfilePhoto}/>
+                        <p class="chat-content">This is a test message</p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <input placeholder="Type your message here..." type="text"></input>
+                    <button class="send-chat-message"><img src={Send}/></button>
+                </div>
             </div>
-            <div class="chat-message from-me">
-                <img class="profile-photo" src="./assets/defaultprofile.png"/>
-                <p class="chat-content">This is a test message</p>
-            </div>
-            <div class="chat-message from-me">
-                <img class="profile-photo" src="./assets/defaultprofile.png"/>
-                <p class="chat-content">This is a test message</p>
-            </div>
-            <div class="chat-message from-me">
-                <img class="profile-photo" src="./assets/defaultprofile.png"/>
-                <p class="chat-content">This is a test message</p>
-            </div>
-            <div class="chat-message from-me">
-                <img class="profile-photo" src="./assets/defaultprofile.png"/>
-                <p class="chat-content">This is a test message</p>
-            </div>
-            <div class="chat-message from-me">
-                <img class="profile-photo" src="./assets/defaultprofile.png"/>
-                <p class="chat-content">This is a test message</p>
-            </div>
-            <div class="chat-message from-me">
-                <img class="profile-photo" src="./assets/defaultprofile.png"/>
-                <p class="chat-content">This is a test message</p>
-            </div>
-            <div class="chat-message from-me">
-                <img class="profile-photo" src="./assets/defaultprofile.png"/>
-                <p class="chat-content">This is a test message</p>
-            </div>
-        </div>
-        <div class="bottom">
-            <input placeholder="Type your message here..." type="text"></input>
-            <button class="send-chat-message"><img src="./assets/send.png"/></button>
-        </div>
-    </div>
         )
     }
 }
--- src/components/floatingbar/floatingbar.js
+++ src/components/floatingbar/floatingbar.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ChatIcon from '../assets/chat_icon.png';
+
+
 
 export default class FloatingBar extends Component {
     render() {
         return (
             <section class="floating bottom">
                 <button class="wheelspin wheelActivate"></button>
-                <button class="chat chatActivate"><img src="./assets/chat_icon.png"/></button>
+                <button class="chat chatActivate"><img src={ChatIcon}/></button>
             </section>
         )
     }
