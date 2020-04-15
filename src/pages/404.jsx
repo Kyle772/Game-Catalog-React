51d57ad705d066a7cd32cf43747edeb0f25325db
Kyle D <contact@KyleDiggs.com>
2020-04-14 20:56:42 -0700

    Added 404 styling

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../components/404/404.scss'
+import logoicon from '../assets/logoicon.png';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className="missing container">
             <h1>404!</h1>
+            <img src={logoicon}/>
             <h2>Page not found</h2>
         </div>
     );
